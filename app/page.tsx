3eb3a88b62ffc3b99e8e9ee7e1acd34aa2b059a6
Anthony Salemo <ajssalemo@gmail.com>
2026-02-04 12:46:19 -0500

    changing background position

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div>
-      <div className="flex min-h-screen items-center justify-center font-sans bg-[url('/background.jpg')] bg-cover h-screen">
+      <div className="flex min-h-screen items-center justify-center font-sans bg-[url('/background.jpg')] bg-cover h-screen bg-top">
         <div className="flex flex-col bg-gray-900 w-1/2 h-96 rounded-sm bg-linear-to-r/srgb from-indigo-950 to-teal-950 justify-center text-white p-8 opacity-90">
           <h1 className="text-center"><span className="text-2xl md:text-4xl">Hi, my name is </span><span className="text-2xl md:text-4xl font-bold italic">Anthony Salemo</span></h1>
           <div className="flex justify-around mt-4 pt-8">
